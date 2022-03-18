66d3b27a36a48c744a7131ad8532cdad681bc0f7
June Kim <godlyjune@gmail.com>
2022-03-18 18:47:18 +0900

    Add openzeppelin contract imports in comment

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,6 +1,9 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "hardhat/console.sol";
 
 contract Greeter {
